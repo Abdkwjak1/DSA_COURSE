2dbee2366ce8d8d3dbbddc20ca03d26a3bcd95dc
Abd Kwjak <abodrus@gmail.com>
2024-02-14 16:36:23 +0100

    modified week_3 money problem

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,9 +1,31 @@
-def change(money):
-    # write your code here
 
-    return money
+#Using greedy algorithm concept, this is an initial solution
+def change(money):
+    num_c = 0
+    while money > 0:
+        money = money - 10 if money >= 10 else money - 5 if money >= 5 else money -1
+        num_c +=1
+    return num_c
+
+#Using Recursion 
+def change_1(money):
+    if money == 0:
+        return 0
+    denominations = [1,5,10]
+    max_c = 0
+    for x in denominations:
+        if  x >= money:
+            max_c = x
+    return change(money-max_c) 
+
+#One-liner solution
+def change_2(money):
+    # * (money//10): determines how many tens there are
+    # * (money%10)//5 : eliminating the tens and checking for fives
+    # * (moeny%5): eliminating the fives and what left are the ones 
+    return(money//10 + (money%10)//5 + money%5)
 
 
 if __name__ == '__main__':
     m = int(input())
-    print(change(m))
+    print(change_1(m))
